fd59f596a95fc9178bba0b73e470eac2dd9448ca
AalosD <166767361+AalosD@users.noreply.github.com>
2025-05-06 14:05:09 -0600

    Add files via upload

--- scripts/scriptAcordeon.js
+++ scripts/scriptAcordeon.js
@@ -1,8 +1,22 @@
-function toggleAccordion(id) {
+/*function toggleAccordion(id) {
     let content = document.getElementById(id);
     if (content.style.display === "block") {
         content.style.display = "none";
     } else {
         content.style.display = "block";
     }
-}
+}*/
+
+function toggleAccordion(id) {
+    // Solo ejecuta si el viewport es menor a 720px
+    if (!window.matchMedia("(max-width: 719px)").matches) {
+      return; // Desactiva en pantallas grandes
+    }
+  
+    let content = document.getElementById(id);
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  }
